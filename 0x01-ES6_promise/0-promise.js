1ee4769570713824298f1cbc98ea59d7ae33f284
Jorshstar <ajosh22.aj@gmail.com>
2023-06-28 00:33:44 +0100

    promises

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,7 +1,7 @@
-function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
+export default function getResponseFromAPI() {
+    const promise = new Promise((resolve, reject) => {
         resolve();
         reject();
     });
+    return promise;
 }
-export default getResponseFromAPI;
